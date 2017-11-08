e1660668491f37a727842ba3c53cf00dcd9889c5
joqk12345 <qianyanqk@163.com>
2017-11-08 10:54:00 +0800

    Initial commit

---
+++ src/main/java/biz/yunduo/HousesCrawler/App.java
@@ -0,0 +1,13 @@
+package biz.yunduo.HousesCrawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
